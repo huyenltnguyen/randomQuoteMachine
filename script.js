4b0dd1d09bcb102e5cea7d91eb1c97e3a5910d3e
Huyen Nguyen <huyenltnguyen@outlook.com>
2017-08-09 20:03:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-var getQuote = function() {
-  $.ajax({
-    url:'https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?',
-    dataType:'json',
-    data:'method=getQuote&format=json&lang=en',
-    
-    // If the request is successful
-    success: function(data) {
-      $('.quote').html(data.quoteText);
-      if (data.quoteAuthor !== '') {
-        $('.author').html('- ' + data.quoteAuthor + ' -');
-      }
-      else {
-        $('.author').html('- Unknown -');
-      }
-    },
-    
-    // If errors occur
-    error: function(xhr, status, error) {
-    $('.quote').text("Oops, something is wrong here. Please try refreshing this page.");
+$(document).ready(function() {
+  init();
+
+  function getQuote() {
+    $.ajax({
+      url:'https://cors-anywhere.herokuapp.com/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        $('.quote').html(data[0].content.slice(0, 3) + '<i class="fa fa-quote-left" aria-hidden="true"></i>' + data[0].content.slice(3));
+        data[0].title ? $('.author').text('- ' + data[0].title) : $('.author').text('- Unknown');
+        console.log(data);
+      },
+      cache: false
+    });
   }
-  });
-};
 
+  function randomColor() {
+    var colors = ['rgb(137, 63, 117)', 'rgb(124, 168, 19)', 'rgb(125, 76, 70)', 'rgb(189, 64, 13)', 'rgb(6, 174, 74)', 'rgb(197, 108, 115)', 'rgb(0, 127, 154)', 'rgb(194, 139, 158)', 'rgb(88, 127, 114)', 'rgb(143, 124, 55)', 'rgb(139, 92, 59)', 'rgb(22, 172, 189)'];
+    var randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
 
-$(document).ready(function(){
-  $('#newQuote').on('click', function(){
+  function setRandomColor(color) {
+    $("body").css("background-color", color);
+    $(".quote").css("color", color);
+    $(".author").css("color", color);
+    $(".btn").css("background-color", color);
+  }
+
+  function init() {
     getQuote();
+    setRandomColor(randomColor());    
+  }
+
+  // EVENT HANDLERS
+  $('#newQuoteBtn').on('click', function(){
+    getQuote();
+    setRandomColor(randomColor()); 
   });
   
-  $('#tweetQuote').on('click', function(){
+  $('#tweetQuoteBtn').on('click', function(){
     window.open("http://twitter.com/intent/tweet?text=" + $('.quote').text())
   });
 });
+
